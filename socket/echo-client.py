f625a1e1c385116ab376341c12bc5a6b4c8bde86
Amin Alipour <amin@aminalipour.com>
2025-09-22 15:23:09 -0500

    Socket coding

---
+++ socket/echo-client.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        message = input("Enter message to send (or 'exit' to quit): ")
+        if message.lower() == 'exit':
+            break
+        s.sendall(message.encode())
+        data = s.recv(1024)
+        print(f"Received {data.decode()!r}")
